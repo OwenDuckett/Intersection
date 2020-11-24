d1e69cf74f6226cafdb41a30f791d8e2a1c319a5
OwenDuckett <67888230+OwenDuckett@users.noreply.github.com>
2020-11-24 19:45:11 +0000

    Init commit

---
+++ shapes/shapes/Collision.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace shapes
+{
+    public static class Collision
+    {
+        /// <summary>
+        /// Checks if two shapes are interseting
+        /// </summary>
+        /// <returns></returns>
+        public static bool DetectIntersection(Rectangle rectangleOne, Rectangle rectangleTwo)
+        {
+            Point rectangleOneTopLeft = rectangleOne._topLeft;
+            Point rectangleTwoTopLeft = rectangleTwo._topLeft;
+
+            if (rectangleOneTopLeft._x + rectangleOne.width >= rectangleTwoTopLeft._x &&    // rectangle one right edge past rectangle two left
+                rectangleOneTopLeft._x <= rectangleTwoTopLeft._x + rectangleTwo.width &&    // rectangle one left edge past rectangle two right
+                rectangleOneTopLeft._y + rectangleOne.height >= rectangleTwoTopLeft._y &&   // rectangle one top edge past rectangle two bottom
+                rectangleOneTopLeft._y <= rectangleTwoTopLeft._y + rectangleTwo.height)     // rectangle one bottom edge past rectangle two top
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public static bool DetectIntersection(Circle circleOne, Circle circleTwo)
+        {
+            Point shapeCenter = circleOne.GetCenter();
+            Point testShapeCenter = circleTwo.GetCenter();
+            // X1 squared - X2 squared + Y1 squared - Y2 squared
+            float totalRadiusSqr = ((circleOne._radius + circleTwo._radius) * (circleOne._radius + circleTwo._radius));
+
+            if (GetDistanceSquared(shapeCenter, testShapeCenter) < totalRadiusSqr)
+                return true; // intersection
+            else
+                return false;
+        }
+
+        public static bool DetectIntersection(Rectangle rectangle, Circle circle)
+        {
+            return DetectIntersection(circle, rectangle);
+        }
+
+        public static bool DetectIntersection(Circle circle, Rectangle rectangle)
+        {
+            Point circleCenter = circle.GetCenter();
+            Point rectangleTopLeft = rectangle._topLeft;
+
+            float testX = circleCenter._x;
+            float testY = circleCenter._y;
+
+            // Find Closest edge
+            if (circleCenter._x < rectangleTopLeft._x)
+                testX = rectangleTopLeft._x;        // left edge
+            else if (circleCenter._x > rectangleTopLeft._x + rectangle.width)
+                testX = rectangleTopLeft._x + rectangle.width;     // right edge
+
+            if (circleCenter._y < rectangleTopLeft._y)
+                testY = rectangleTopLeft._y;        // top edge
+            else if (circleCenter._y > rectangleTopLeft._y + rectangle.height)
+                testY = rectangleTopLeft._y + rectangle.height;     // bottom edge
+
+            float distX = circleCenter._x - testX;
+            float distY = circleCenter._y - testY;
+
+            float distanceSqr = (distX * distX) + (distY * distY);
+            float totalRadiusSqr = circle._radius * circle._radius;
+
+            if (distanceSqr < totalRadiusSqr)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Gets the distance between two points 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static float GetDistanceSquared(Point a, Point b)
+        {
+            return ((a._x - b._x) * (a._x - b._x)) + ((a._y - b._y) * (a._y - b._y));
+        }
+    }
+}
---
+++ shapes/shapes/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace shapes
+{
+    public partial class Form1 : Form
+    {
+        public List<Shape> shapes = new List<Shape>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Rectangle one
+            shapes.Add(new Rectangle(2, new Point(80, 70), new Point(400, 70), new Point(80, 170), new Point(400, 170)));
+
+            // Rectangle two
+            shapes.Add(new Rectangle(3, new Point(130, 120), new Point(150, 120), new Point(130, 1000), new Point(150, 1000)));
+
+            // Cirlce
+            Point[] center = { new Point(70, 70) };
+            Circle circle = new Circle(1, center, 60);
+            shapes.Add(circle);
+
+            // Cirlce
+            Point[] center2 = { new Point(420, 140) };
+            Circle circle2 = new Circle(4, center2, 30);
+            shapes.Add(circle2);
+
+
+            this.Paint += this.Form1_Paint;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Red, 10);
+
+            Rectangle r1 = (Rectangle)shapes[0];
+            System.Drawing.Point[] rectangleTwo = r1.GetSystemPoints();
+            g.DrawPolygon(p, rectangleTwo);
+
+            Rectangle r2 = (Rectangle)shapes[1];
+            System.Drawing.Point[] rectanleThree = r2.GetSystemPoints();
+            g.DrawPolygon(p, rectanleThree);
+            //System.Drawing.Rectangle r = new System.Drawing.Rectangle();
+            //g.DrawRectangle(p, );
+
+            Circle circle = (Circle)shapes[2];
+            //Create location and size of ellipse.
+            int x = circle.GetCoordinates()[0]._x;
+            int y = circle.GetCoordinates()[0]._y;
+            int radius = circle._radius;
+
+            // Draw ellipse to screen.
+            e.Graphics.DrawCircle(p, x, y, radius);
+
+            Circle circle1 = (Circle)shapes[3];
+            //Create location and size of ellipse.
+            int x1 = circle1.GetCoordinates()[0]._x;
+            int y1 = circle1.GetCoordinates()[0]._y;
+            int radius1 = circle1._radius;
+
+            // Draw ellipse to screen.
+            e.Graphics.DrawCircle(p, x1, y1, radius1);
+
+            g.Dispose();
+
+            FindIntersections(shapes);
+        }
+
+        public Dictionary<int, List<int>> FindIntersections(List<Shape> shapes)
+        {
+            Dictionary<int, List<int>> intersectingShapes = new Dictionary<int, List<int>>();
+
+            foreach (Shape shape in shapes)
+            {
+                List<int> shapeIds = shape.GetIntersectingShapes(shapes);
+                intersectingShapes.Add(shape._id, shapeIds);
+            }
+       
+            return intersectingShapes;
+        }
+
+        
+    }
+}
+
+public static class GraphicsExtensions
+{
+    // Created to draw a circle with a radius simply.
+    public static void DrawCircle(this Graphics g, Pen pen,
+                                  float centerX, float centerY, float radius)
+    {
+        g.DrawEllipse(pen, centerX - radius, centerY - radius,
+                      radius + radius, radius + radius);
+    }
+}
